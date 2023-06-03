55bc5619bc27ffb493cc902b2fcfa2f6aac752e0
Sadia Nawsin <sadianawsin018@gmail.com>
2023-06-03 18:53:10 +0600

    Add files via upload

---
+++ Assignments/operator_preceedence.php
@@ -0,0 +1,27 @@
+<?php
+$now = 2023;
+ $ageKarim = $now - 1991;//32
+$ageRahim = $now - 2018;//5
+
+
+
+
+$averageAge = $ageKarim + $ageRahim / 2;
+echo $averageAge;
+echo "\n";
+
+$averageAge = ($ageKarim + $ageRahim) / 2;
+
+echo $averageAge;
+ echo "\n";
+
+$age1 = 12;  
+$age2 = 50;
+
+echo $age1 + $age2;
+
+
+?>
+
+
+
